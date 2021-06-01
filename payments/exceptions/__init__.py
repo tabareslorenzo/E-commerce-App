1d386d5104904e443fc4d987723b7ee47384159f
tabareslorenzo <10tabareslorenzo@gmail.com>
2021-06-01 09:12:01 -0400

    na

---
+++ payments/exceptions/__init__.py
@@ -0,0 +1,23 @@
+class OrderDoesNotExistsException(Exception):
+    message = "Order Does not Exist!"
+    def __init__(self):
+        super().__init__(OrderDoesNotExistsException.message)
+    @staticmethod
+    def get_message():
+        return OrderDoesNotExistsException.message
+
+class UserNotOwnerException(Exception):
+    message = "You do not own this order!"
+    def __init__(self):
+        super().__init__(UserNotOwnerException.message)
+    @staticmethod
+    def get_message():
+        return UserNotOwnerException.message
+
+class CanceledOrderException(Exception):
+    message = "Order was!"
+    def __init__(self):
+        super().__init__(CanceledOrderException.message)
+    @staticmethod
+    def get_message():
+        return CanceledOrderException.message
